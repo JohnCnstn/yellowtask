6e39e3bbf8d340ae2c19cb02efad71cda1c44aff
Pavel <crystalfaster@gmail.com>
2018-02-14 18:52:32 +0300

    add path variables

--- src/main/java/com/johncnstn/controller/HomeController.java
+++ src/main/java/com/johncnstn/controller/HomeController.java
@@ -1,12 +1,18 @@
 package com.johncnstn.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    @Value("#{systemEnvironment['MYSQL_DB_PORT']}")
+    private String javaHome;
+
     @GetMapping("/home")
     public String home() {
+        System.out.println(javaHome);
         return "/home";
     }
 }
