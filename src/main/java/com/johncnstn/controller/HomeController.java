de73818423a6cc9054a0d746e01cb9fd798c7068
Pavel <crystalfaster@gmail.com>
2018-02-17 03:04:41 +0300

    add jasper reports lib

--- src/main/java/com/johncnstn/controller/HomeController.java
+++ src/main/java/com/johncnstn/controller/HomeController.java
@@ -1,18 +1,48 @@
 package com.johncnstn.controller;
 
-import org.springframework.beans.factory.annotation.Value;
+import com.johncnstn.data.entity.Entry;
+import com.johncnstn.report.CurrencyRate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.Currency;
+import java.util.Date;
+import java.util.List;
 
 @Controller
 public class HomeController {
 
-    @Value("#{systemEnvironment['MYSQL_DB_PORT']}")
-    private String javaHome;
-
     @GetMapping("/home")
     public String home() {
-        System.out.println(javaHome);
         return "/home";
     }
+
+    @GetMapping("/exchangeRates")
+    public String handleForexRequest(Model model) {
+        model.addAttribute("todayCurrencyRates", getTodayForexRates());
+        return "forexView";
+    }
+
+    private List<CurrencyRate> getTodayForexRates() {
+        //dummy rates
+        List<CurrencyRate> currencyRates = new ArrayList<>();
+        Date today = new Date();
+        List<Currency> currencies = new ArrayList<>(Currency.getAvailableCurrencies());
+
+        CurrencyRate cr = new CurrencyRate();
+        cr.setDate(today);
+
+        currencyRates.add(cr);
+
+//        for (int i = 0; i < currencies.size(); i += 2) {
+//            CurrencyRate cr = new CurrencyRate();
+//            cr.setDate(today);
+//
+//            currencyRates.add(cr);
+//        }
+        return currencyRates;
+    }
+
 }
---
+++ src/main/java/com/johncnstn/report/CurrencyRate.java
@@ -0,0 +1,12 @@
+package com.johncnstn.report;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+public class CurrencyRate {
+    @Setter
+    @Getter
+    private Date date;
+}
